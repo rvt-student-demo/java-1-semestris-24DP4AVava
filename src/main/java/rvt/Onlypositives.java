1e8f8abcc2b34b9249368b189c00c63927af7e54
Mainoks <a240308av@rvt.lv>
2025-10-04 17:28:01 +0300

    .

---
+++ src/main/java/rvt/Onlypositives.java
@@ -0,0 +1,27 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Onlypositives {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+        System.out.println("Input a number");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number == 0) {
+            break;
+        }
+
+        if (number < 0) {
+            System.out.println("Unfit number");
+            continue;
+        }
+
+        System.out.println(number * number);
+    }
+
+    }
+}
